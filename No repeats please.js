3cb5bc59d2e0b9ff006c90e5f4ecd06e0fd71d3e
Alex <alex.alexson143@gmail.com>
2017-01-26 21:46:31 +0800

    Update and rename No repeats please to No repeats please.js

--- No repeats please.js
+++ No repeats please.js
@@ -4,7 +4,15 @@ function permAlone(str) {
   var nonRepeat = 0;
   var strArray = str.split('');
   
+  // Loop through all letters
   nonRepeat = strArray.reduce(function(total, curLetter, index, origArr) {
+    var workingArr = origArr;
+    var loop = 1;
+    
+    // compute (n-1)!
+    for(i = 1; i < origArr.length; ++i) {
+      loop *= i;
+    }
     
     return total;
   }, 0);
