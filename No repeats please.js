6008917a6275c04bf9a72751ef9e508410e612f2
Alex <alex.alexson143@gmail.com>
2017-01-26 22:31:37 +0800

    Update No repeats please.js

--- No repeats please.js
+++ No repeats please.js
@@ -3,15 +3,21 @@ function permAlone(str) {
   console.log('@@@@@@@@@@');
   var nonRepeat = 0;
   var strArray = str.split('');
+  var loop = 1;
+  var i = 0;
+  
+  // compute (n-1)!
+  for(i = 1; i < strArray.length; ++i) {
+      loop *= i;
+  }
   
   // Loop through all letters
   nonRepeat = strArray.reduce(function(total, curLetter, index, origArr) {
     var workingArr = origArr;
-    var loop = 1;
     
-    // compute (n-1)!
-    for(i = 1; i < origArr.length; ++i) {
-      loop *= i;
+    // loop curLetter (n-1)! times
+    for(i = 0; i < loop; ++i) {
+      console.log(loop);
     }
     
     return total;
