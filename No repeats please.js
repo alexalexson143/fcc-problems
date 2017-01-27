6a138beabb7db68cd8c6db47350630b18045d637
Alex <alex.alexson143@gmail.com>
2017-01-27 20:17:30 +0800

    Update No repeats please.js

--- No repeats please.js
+++ No repeats please.js
@@ -3,14 +3,23 @@ function permAlone(str) {
   console.log('@@@@@@@@@@');
   var nonRepeat = 0;
   var strArray = str.split('');
+  var consecutive = /([a-z])\1/;
   
-  function permuteAll(orgArray, partArray, excessArray, iteration) {
+  function permuteAll(orgArray, excessArray, partArray, iteration) {
+    var testArray = [];
+    var withConsecutive = false;
+    
     if(partArray.length > 2) {
-      
+      permuteAll(orgArray, orgArray.slice(0, iteration), partArray.slice(1), ++iteration);
+    } else {
+      testArray = excessArray.concat(partArray);
+      withConsecutive = consecutive.test(testArray.join(''));
     }
   }
   
-  permuteAll(strArray, strArray, strArray, 0);
+  strArray.forEach(function(letter) {
+    permuteAll(strArray, strArray, strArray, 1);
+  });
   
   //return nonRepeat;
 }
