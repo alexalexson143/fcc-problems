6c9915e67e34a6502b01e15651eefc4052dfe526
Alex <alex.alexson143@gmail.com>
2017-01-27 20:06:27 +0800

    Update No repeats please.js

--- No repeats please.js
+++ No repeats please.js
@@ -3,25 +3,14 @@ function permAlone(str) {
   console.log('@@@@@@@@@@');
   var nonRepeat = 0;
   var strArray = str.split('');
-  var loop = 1;
-  var i = 0;
   
-  // compute (n-1)!
-  for(i = 1; i < strArray.length; ++i) {
-      loop *= i;
+  function permuteAll(orgArray, partArray, excessArray, iteration) {
+    if(partArray.length > 2) {
+      
+    }
   }
   
-  // Loop through all letters
-  nonRepeat = strArray.reduce(function(total, curLetter, index, origArr) {
-    var workingArr = origArr;
-    
-    // loop curLetter (n-1)! times
-    for(i = 0; i < loop; ++i) {
-      console.log(loop);
-    }
-    
-    return total;
-  }, 0);
+  permuteAll(strArray, strArray, strArray, 0);
   
   //return nonRepeat;
 }
