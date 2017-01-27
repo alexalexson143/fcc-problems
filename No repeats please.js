8f82cd9e859b30aeab7a25695b5f13fd1af67233
Alex <alex.alexson143@gmail.com>
2017-01-27 21:17:20 +0800

    FINALLY!!! AFTER THREE DAYS!

--- No repeats please.js
+++ No repeats please.js
@@ -1,43 +1,30 @@
 
 function permAlone(str) {
-  console.log('@@@@@@@@@@');
   var nonRepeat = 0;
   var strArray = str.split('');
   var consecutive = /([a-z])\1/;
   
   function permuteAll(partArray, excessArray) {
-    var sliceArray = [];
-    var wholeArray = [];
     var testArray = [];
     var withConsecutive = false;
     
-    sliceArray = sliceArray.concat(partArray);
-    wholeArray = excessArray.concat(sliceArray);
-    
-    sliceArray.forEach(function(letter) {
-      if(sliceArray.length > 2) {
-        permuteAll(sliceArray.slice(1), wholeArray.concat(sliceArray[0]));
+    partArray.forEach(function(letter) {
+      if(partArray.length > 2) {
+        permuteAll(partArray.slice(1), excessArray.concat(partArray[0]));
       } else {
-        testArray = excessArray.concat(sliceArray);
-        withConsecutive = consecutive.test(testArray.join(''));
-        if(!withConsecutive) {
-          ++nonRepeat;
-        }
-        sliceArray.reverse();
         testArray = excessArray.concat(partArray);
         withConsecutive = consecutive.test(testArray.join(''));
         if(!withConsecutive) {
           ++nonRepeat;
         }
-        return 0;
       }
       partArray.push(partArray.shift());
     });
   }
   
-  permuteAll(strArray);
+  permuteAll(strArray, []);
   
-  //return nonRepeat;
+  return nonRepeat;
 }
 
-//permAlone('aab');
+permAlone('aaabb');
